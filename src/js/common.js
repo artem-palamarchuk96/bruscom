fa57ff326d3eb498143f91cd91f81e07feef27e5
a.palamarchuk <artem.palamarchuk.96@gmail.com>
2016-07-10 21:44:34 +0300

    create partial views

---
+++ src/js/common.js
@@ -0,0 +1,5 @@
+//= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/bootstrap/dist/js/bootstrap.js
+//= ../../bower_components/fancybox/source/jquery.fancybox.pack.js
+//= ../../bower_components/nouislider/distribute/nouislider.js
+
